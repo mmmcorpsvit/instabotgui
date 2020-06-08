d9d6634925d4e7044a5a1a342116a793f0a2906d
Николай Инкогнито <mmmsvit@gmail.com>
2020-06-08 20:59:07 +0300

    fix

--- __main__.py
+++ __main__.py
@@ -18,13 +18,15 @@ from PyQt5.QtQml import QQmlApplicationEngine
 
 from PyQt5.QtWidgets import QTreeView, QVBoxLayout, \
     QApplication, QListWidget, QMainWindow, QPlainTextEdit, QPushButton, QTextEdit, QWidget, QGridLayout, QLabel, \
-    QLineEdit, QToolBar, QAction, qApp, QDesktopWidget, QSizePolicy
+    QLineEdit, QToolBar, QAction, qApp, QDesktopWidget, QSizePolicy, QFileDialog
 from PyQt5.uic import loadUi  # noqa
 
 from pyqtgraph.parametertree import ParameterTree
 from pyqtgraph.parametertree import Parameter as ParameterForTree
 
 import qdarkstyle
+
+from core.bot import insta_clone, InstaPyStartStageItem, InstaPyEndStageItem, get_actions_list, ACTIONS_LIST
 
 os.environ['PYQTGRAPH_QT_LIB'] = 'PyQt5'
 
@@ -344,20 +346,30 @@ class Example(QWidget):
 
 
 class Example2(QMainWindow):
-    toolbar: QToolBar
+    # toolbar: QToolBar
 
     def __init__(self):
         super().__init__()
+        self.filterTypes = 'InstapyBot files (*.insta_json);;'
 
-        self.initUI()
+        # ------------------
+        self.setGeometry(200, 200, 1200, 800)
+        # self.setSizePolicy()
+        self.setWindowTitle('InstagramBot v0.1')
+        self.setWindowIcon(QIcon('assets/icon.png'))
+        self.center()
 
-    def initUI(self):
-        # actions
+        # ------------------ actions
         action_new_file = QAction(QIcon('assets/new.svg'), 'New', self)
         # action_new_file = QAction(QIcon.fromTheme("SP_FileIcon"), 'New', self)
         action_new_file.setShortcut('Ctrl+N')
         # action_new_file.triggered.connect(qApp.quit)
         action_new_file.triggered.connect(self.action_new_file)
+
+        # action_load_file
+        action_open_file = QAction(QIcon('assets/open.png'), 'Open', self)
+        action_open_file.setShortcut('Ctrl+O')
+        action_open_file.triggered.connect(self.action_open_file)
 
         action_save_file = QAction(QIcon('assets/save.png'), 'Save', self)
         action_save_file.setShortcut('Ctrl+S')
@@ -377,9 +389,10 @@ class Example2(QMainWindow):
         action_stage_run.setShortcut('F5')
         action_stage_run.triggered.connect(self.action_stage_run)
 
-        action_stage_stop = QAction(QIcon('assets/stop.png'), 'Run', self)
+        action_stage_stop = QAction(QIcon('assets/stop.png'), 'Stop', self)
         action_stage_stop.setShortcut('F9')
         action_stage_stop.setDisabled(True)
+        action_stage_stop.setVisible(False)
         action_stage_stop.triggered.connect(self.action_stage_stop)
 
         # ---------------------
@@ -387,9 +400,13 @@ class Example2(QMainWindow):
         # self.spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         self.toolbar = self.addToolBar('New')
-        self.addToolBar(QtCore.Qt.ToolBarArea(QtCore.Qt.TopToolBarArea), self.toolbar)
+        self.toolbar.setMovable(False)
+        # self.addToolBar(QtCore.Qt.ToolBarArea(QtCore.Qt.TopToolBarArea), self.toolbar)
 
         self.toolbar.addAction(action_new_file)
+        self.toolbar.addSeparator()
+
+        self.toolbar.addAction(action_open_file)
         self.toolbar.addSeparator()
 
         self.toolbar.addAction(action_save_file)
@@ -397,48 +414,69 @@ class Example2(QMainWindow):
         # self.toolbar.addWidget(self.spacer)
         self.toolbar.addSeparator()
 
-        # --------------------
         self.toolbar.addAction(action_stage_run)
         self.toolbar.addAction(action_stage_stop)
         # self.toolbar.addWidget(self.spacer)
         self.toolbar.addSeparator()
 
-        # --------------------
         self.toolbar.addAction(action_help)
-        # self.toolbar.setAlignment(Qt.AlignRight | Qt.AlignBottom)
 
-        # toolbar = QToolBar()
-        # newButton = QToolButton()
+        # --------------------
+        self.actions_list = QListWidget()
+        self.actions_list.doubleClicked.connect(self.actions_listDoubleClicked)
 
-        title = QLabel('Title')
-        author = QLabel('Author')
-        review = QLabel('Review')
+        self.stages_list = QListWidget()
+        self.stages_list.doubleClicked.connect(self.stages_listDoubleClicked)
+        self.stages_list.itemSelectionChanged.connect(self.stages_listitemSelectionChanged)
 
-        titleEdit = QLineEdit()
-        authorEdit = QLineEdit()
-        reviewEdit = QTextEdit()
+        self.properties_tree = ParameterTree()
+        self.log_textEdit = QTextEdit()
 
+        # --------------------
         self.grid = QGridLayout()
         self.grid.setSpacing(10)
 
-        self.grid.addWidget(title, 1, 0)
-        self.grid.addWidget(titleEdit, 1, 1)
+        self.grid.addWidget(QLabel('Actions'), 0, 0)
+        self.grid.addWidget(self.actions_list, 1, 0)
 
-        self.grid.addWidget(author, 2, 0)
-        self.grid.addWidget(authorEdit, 2, 1)
+        self.grid.addWidget(QLabel('Stages'), 0, 1)
+        self.grid.addWidget(self.stages_list, 1, 1)
 
-        self.grid.addWidget(review, 3, 0)
-        self.grid.addWidget(reviewEdit, 3, 1, 5, 1)
+        self.grid.addWidget(QLabel('Properties'), 0, 2)
+        self.grid.addWidget(self.properties_tree, 1, 2)
 
+        self.grid.addWidget(QLabel('Logs'), 2, 0)
+        self.grid.addWidget(self.log_textEdit, 3, 0, 1, 3)
+
+        self.grid.setRowStretch(1, 300)
+
+        self.grid.setColumnStretch(0, 10)
+        self.grid.setColumnStretch(2, 10)
         self.setLayout(self.grid)
 
+        widget = QWidget()
+        widget.setLayout(self.grid)
+        self.setCentralWidget(widget)
 
-        self.setGeometry(200, 200, 1000, 600)
-        # self.setSizePolicy()
-        self.setWindowTitle('InstagramBot v0.1')
-        self.setWindowIcon(QIcon('assets/icon.png'))
-        self.center()
+        # --------------------
+        sys.stdout = Stream(newText=self.onUpdateText)
+        sys.stderr = Stream(newText=self.onUpdateText)
+
+        self.actions_list.addItems(get_actions_list())
+        self.action_new_file()
+
         self.show()
+
+    def onUpdateText(self, text):
+        cursor = self.log_textEdit.textCursor()
+        cursor.movePosition(QTextCursor.End)
+        cursor.insertText(text)
+        self.log_textEdit.setTextCursor(cursor)
+        self.log_textEdit.ensureCursorVisible()
+
+    def __del__(self):
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
 
     def center(self):
         qr = self.frameGeometry()
@@ -447,10 +485,175 @@ class Example2(QMainWindow):
         self.move(qr.topLeft())
 
     def action_new_file(self):
+        self.stages_list.clear()
+        self.stages_list.addItem(insta_clone(InstaPyStartStageItem))
+        self.stages_list.addItem(insta_clone(InstaPyEndStageItem))
+        self.stages_list.setCurrentRow(0)
+        self.actions_list.setCurrentRow(0)
+        self.log_textEdit.clear()
+        logging.info('Welcome to InstaBot v0.1---------------')
+        print('Welcome to InstaBot v0.1**************')
         pass
 
-    def action_stage_run(self):
+    def actions_listDoubleClicked(self, qmodelindex):  # noqa
+        # add new action to stages list
+        index = self.actions_list.currentRow()
+        _new_object = insta_clone(ACTIONS_LIST[index])
+
+        self.stages_list.insertItem(self.stages_list.count() - 1, _new_object)
         pass
+
+    def stages_listDoubleClicked(self, qmodelindex):  # noqa
+        # remove action from stages list
+        index = self.stages_list.currentRow()
+        item = self.stages_list.item(index)
+
+        if item.object.name in ['__init__', 'end']:
+            logging.error(f'You cannot remove stage: {item.object.name}')
+            return None
+        self.stages_list.takeItem(index)
+        pass
+
+    def action_open_file(self):
+        path, _ = QFileDialog.getOpenFileName(parent=self, caption='Open file', directory='', filter=self.filterTypes)
+
+        if path:
+            try:
+                with open(path, 'r') as f:
+                    text = f.read()
+                    f.close()
+            except Exception as e:
+                self.dialog_message(str(e))
+            else:
+                self.path = path
+                self.editor.setPlainText(text)
+                self.update_title()
+
+    def properties_tree_change(self, param, changes):
+        # print("tree changes:")
+        for param2, change, data in changes:
+            index = self.stages_list.currentRow()
+            data2 = self.stages_list.item(index).object.values
+
+            data2[param2.opts['name']] = data
+
+    def stages_listitemSelectionChanged(self):
+        # render new propertry list table
+        index = self.stages_list.currentRow()
+        private_data = self.stages_list.item(index).object.anotation_call
+        private_values_list = self.stages_list.item(index).object.values
+
+        params_list = []
+        for x in private_data:
+            el = private_data[x]
+            if el.name == 'self':
+                continue
+            new_el = {'name': el.name, 'default': el.default}
+
+            if el.annotation.__name__ not in ['_empty']:
+                if el.annotation.__name__ == 'list':
+                    new_el['type'] = 'text'
+                else:
+                    new_el['type'] = el.annotation.__name__
+            else:
+                pass
+
+            # set value from stored in Item-object list
+            if el.name in private_values_list:
+                new_el['value'] = private_values_list[el.name]
+
+            if hasattr(el, 'value'):
+                new_el['value'] = getattr(el, 'value')
+
+            params_list.append(new_el)
+
+        self.current_stage = [
+            {'name': 'Basic parameter data types', 'type': 'group', 'children':
+                params_list
+             }]
+
+        # params = data
+
+        # p = ParameterForTree.create(name='params', type='group', children=self.current_stage)
+        p = ParameterForTree.create(name='params', type='list', children=self.current_stage)
+
+        # p.sigTreeStateChanged.connect(None)
+        self.properties_tree.setParameters(p, showTop=False)
+        p.sigTreeStateChanged.connect(self.properties_tree_change)
+
+    def action_stage_run(self):
+        logging.info('Start working')
+        # --------------------------------------------
+
+        # create tasks list (just do deep copy)
+        y = [self.stages_list.item(x).object for x in range(self.stages_list.count())]
+        stages = deepcopy(y)
+
+        if len(stages) < 2 or stages[0].name != '__init__' or stages[len(stages) - 1].name != 'end':
+            logging.error('First stage must be init, last stage must be end')
+            return None
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        # gecko_driver_path = None
+
+        x = stages[0].values
+        x['username'] = 'gloss_dp'
+        x['password'] = 'UKJCwev'
+
+        x['split_db'] = True
+
+        # rewrite if default some settings
+        if 'browser_executable_path' not in x:
+            x['browser_executable_path'] = os.path.join(dir_path, 'firefox/firefox.exe')
+            # x['browser_executable_path'] = os.path.join(dir_path, 'FirefoxPortable32\\FirefoxPortable.exe')
+
+        if 'geckodriver_path' not in x:
+            # gecko_driver_path = tempfile.mkdtemp()
+            # copyfile(os.path.join(dir_path, 'geckodriver.exe'), os.path.join(gecko_driver_path, 'geckodriver.exe'))
+            x['geckodriver_path'] = os.path.join(dir_path, 'geckodriver.exe')
+            # x['geckodriver_path'] = os.path.join(gecko_driver_path, 'geckodriver.exe')
+
+        if 'want_check_browser' not in x:
+            x['want_check_browser'] = False
+
+        # session = InstaPy(username='gloss_dp',
+        #                   password='UKJCwev',
+        #                   browser_executable_path=r"D:\Program Files444\firefox.exe")
+
+        from instapy import InstaPy, smart_run, set_workspace
+        session = InstaPy(**x)
+
+        # with smart_run(session, threaded=True):
+        with smart_run(session):
+            # Available character sets: LATIN, GREEK, CYRILLIC, ARABIC, HEBREW, CJK, HANGUL, HIRAGANA, KATAKANA and THAI
+            session.set_mandatory_language(enabled=True, character_set=['LATIN', 'CYRILLIC'])
+
+            for index, stage in enumerate(stages):
+                # current_values = deepcopy(stage.values)
+
+                # skip init and end stages
+                if index == 0 or index == len(stages):
+                    continue
+
+                # convert text (valid for propretry editor) to list
+                current_values = {
+                    key: value.split() if stage.anotation_call[key].annotation.__name__ == 'list' else value for
+                    (key, value) in stage.values.items()}
+
+                # call function from instance
+                f = getattr(session, stage.name)
+                f(**current_values)  # noqa https://youtrack.jetbrains.com/issue/PY-27935
+                pass
+
+        # x = stages[len(stages)].value
+        # insta.end()
+
+        # ------------------------
+        # if gecko_driver_path:
+        #     shutil.rmtree(gecko_driver_path)
+        # --------------------------------------------
+
+        logging.info('End working')
 
     def action_stage_stop(self):
         pass
